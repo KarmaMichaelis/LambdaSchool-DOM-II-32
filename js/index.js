6e841c1e40c978af7fc4cc734a19ca0a73fe6d0a
KamaMichaelis <achantayd@gmail.com>
2020-06-04 00:15:12 -0400

    two events added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+
+//changes big bus image
+const busImage=document.querySelector('.intro img')
+
+function imageSwitch(event){
+    busImage.setAttribute('src','https://media.giphy.com/media/J4tKwYHp0DFC8MEY2K/giphy.gif')
+}
+
+busImage.addEventListener('click',imageSwitch)
+
+//////next event
+//changes logo style
+const logoRotation=document.querySelector('.logo-heading')
+
+
+logoRotation.addEventListener('mouseover',function(event){
+    event.target.style.transform='rotate(75deg) '
+    event.target.style.color='yellow'
+})
+
+//next event
